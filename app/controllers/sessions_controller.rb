8ce38330812c9dd0cde4d44a7870c6a53bbe9b45
Damien Milon <milon.damien@gmail.com>
2020-04-17 12:30:08 +0200

    sync branches

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,17 @@ require_relative "../views/sessions_view"
 
 class SessionsController
   def initialize(employee_repository)
-    @session_view = SessionsView.new
+    @sessions_view = SessionsView.new
     @employee_repository = employee_repository
   end
 
   def login
-    username = @session_view.ask_for(:username)
-    password = @session_view.ask_for(:password)
+    username = @sessions_view.ask_for(:username)
+    password = @sessions_view.ask_for(:password)
     employee = @employee_repository.find_by_username(username)
     return employee if employee && employee.password == password
 
-    @session_view.print_wrong_credentials
+    @sessions_view.print_wrong_credentials
     login
   end
 end
