a163730c0318c76ff2dc71ca819e3b40ade08ffe
Anne de Joly <anne.dejoly@edhec.com>
2022-01-26 14:11:34 +0000

    Update with keyword arguments

--- app/repositories/order_repository.rb
+++ app/repositories/order_repository.rb
@@ -35,8 +35,7 @@ class OrderRepository
   private
 
   def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_file, csv_options) do |row|
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |row|
       row[:id] = row[:id].to_i
       row[:delivered] = row[:delivered] == "true"
       row[:meal] = @meal_repository.find(row[:meal_id].to_i)
